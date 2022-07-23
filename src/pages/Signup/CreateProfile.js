1e5d4a56dd870586d751e71a8811d38d2d2f3b88
Kanghyeon Nam <andy0011@naver.com>
2022-07-23 20:51:50 +0900

    불필요한 imp 제거 (warning 제거)

--- src/pages/Signup/CreateProfile.js
+++ src/pages/Signup/CreateProfile.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { dbService, storageService } from 'fbase';
 import { doc, setDoc } from '@firebase/firestore';
 import { ref, uploadString, getDownloadURL } from '@firebase/storage';
